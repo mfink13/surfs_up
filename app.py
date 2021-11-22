c6af69c7a9c81e8b63354f197607f379a83f4322
Megan Fink <finkers13.com>
2021-11-22 12:03:05 -0600

    flask started

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask
+
+#create a new Flask app instance
 app = Flask(__name__)
+
+#create flask routes
 @app.route('/')
+
+#create a function called hello_world()
 def hello_world():
     return 'Hello world'
+# local host for output http://127.0.0.1:5000/
+
